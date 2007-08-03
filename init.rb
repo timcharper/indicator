787e3a7f1af245fa558e541a59dc477da53417ef
timcharper <timcharper@072a2ee3-c236-0410-8cf8-11b756d0386d>
2007-08-03 04:06:40 +0000

    first import
    git-svn-id: http://indicator.googlecode.com/svn/trunk@2 072a2ee3-c236-0410-8cf8-11b756d0386d

---
+++ init.rb
@@ -0,0 +1,9 @@
+load "indicator.rb"
+
+ActionView::Base.send(:include, Indicator::InstanceMethods)
+# install files
+['/public/images'].each{|dir|
+  source = File.join(directory,dir)
+  dest = RAILS_ROOT + dir
+  FileUtils.cp_r(Dir.glob(source+'/*.*'), dest)
+} unless File.exists?(RAILS_ROOT + '/public/images/indicator.gif')
---
+++ lib/indicator.rb
@@ -0,0 +1,79 @@
+class Indicator
+  module InstanceMethods
+    
+    def indicator_id(identifier)
+      "#{identifier}_indicator"
+    end
+  
+    def indicator(identifier)
+      image_tag "indicator.gif", :id => indicator_id(identifier), :style => "display:none"
+    end  
+    
+    # returns the options for an ajax call to hide/show indicator and div element.  Use in conjunction with div_for_with_indicator for best results
+    # Example
+    # 
+    # 
+    def indicated_update(record, prefix=nil, options={})
+      indicated_update_options(record, prefix).merge(options)
+    end
+    
+    alias :indicated :indicated_update
+      
+    def show_indicator(identifier)
+      "$('#{indicator_id(identifier)}').show();"
+    end
+    
+    def hide_indicator(identifier)
+      "$('#{indicator_id(identifier)}').hide();"
+    end
+    
+    def indicate_loading(identifier)
+      "#{show_indicator(identifier)} Element.hide( '#{identifier}' );"
+    end
+    
+    def indicate_complete(identifier)
+      "#{hide_indicator(identifier)} Element.show( '#{identifier}' );"
+    end
+    
+    def indicated_tag(tag, identifier, *args, &block)
+      content = args.first.is_a?(Hash) ? "" : args.shift
+      options = args.first.is_a?(Hash) ? args.shift : {}
+
+      output = indicator(identifier)
+      output << if block_given? 
+        content_tag(tag, capture(&block), options.merge(:id => identifier)) 
+      else
+        content_tag(tag, content, options.merge(:id => identifier) )
+      end
+      
+      concat output, block.binding if block_given?
+      output
+    end
+    
+    def indicated_div(identifier, *args, &block)
+      indicated_tag(:div, identifier, *args, &block)
+    end
+    
+    def indicated_span(identifier, *args, &block)
+      indicated_tag(:span, identifier, *args, &block)
+    end
+  end
+end
+
+module ActionView::Helpers::PrototypeHelper
+  def remote_function_with_indicator(options)
+    indicate, indicate_and_update = options.delete(:indicate), options.delete(:indicate_and_update)
+    identifier = indicate || indicate_and_update
+    
+    if identifier
+      options[:update] = identifier if indicate_and_update
+      for op in [:loading, :complete]
+        options[op] = send("indicate_#{op}", identifier) + (options[op] ? (";" + options[op].to_s) : "")
+      end
+    end
+    
+    remote_function_without_indicator(options)
+  end
+  
+  alias_method_chain :remote_function, :indicator
+end
